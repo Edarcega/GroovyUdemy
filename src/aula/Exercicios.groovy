0fe82fb9d95cf7f0a343edbc2fa0d55efcd999a5
edimar <edimar.eds@gmail.com>
2025-07-28 08:32:28 -0300

    Projeto atualizado até a aula 17

--- src/aula/Exercicios.groovy
+++ src/aula/Exercicios.groovy
@@ -1,6 +1,7 @@
 package aula
 
 import classes.Cliente
+import classes.ProdutoGroovy
 import org.junit.Test
 
 class Exercicios {
@@ -23,11 +24,13 @@ class Exercicios {
         c.setNome "Zéxinho"
         c.setData new Date()
         c.somar(1, 10)
+        println(c.getNome() + " - " + c.getData())
     }
 
     @Test
+    //Constructor Names Arguments
     // Maper cpnstructor
-    void exercioco3() {
+    void exercicio3() {
         Cliente c = new Cliente()
         println c.getNome() + " - " + c.getData()
 
@@ -37,9 +40,27 @@ class Exercicios {
         c = new Cliente(data: new Date())
         println c.getNome() + " - " + c.getData()
 
+        // Com varios mapeamentos de variaveis do objeto
         c = new Cliente(nome: "Luizinho", data: new Date())
         println c.getNome() + " - " + c.getData()
-
     }
 
+    @Test
+    // Parei aqui, em 3:59 minutos do vídeo da aula 7
+    void exercicio4() {
+        Cliente c = new Cliente(nome: "Fernando", data: new Date())
+        println c["nome"] // Pega o dado do atributo a partir de uma string, caso ele exista
+        println c['data'] // Interessante que funciona assim com aspas simples também
+        c["nome"] = "Marcão" // Atribui dado a um atributo a partir de uma string, caso ela exista.
+        println c["nome"]
+    }
+
+    @Test
+    void exercicio5() {
+        ProdutoGroovy p = new ProdutoGroovy(nome: "CD",valor: 10.00)
+        println(p.nome) // Acesso direto sem necessidade de usar o get ou set
+        println(p.valor)
+        p.valor = 20.00 // Atiubuição direta sem uso do set
+        println(p.valor)
+    }
 }
